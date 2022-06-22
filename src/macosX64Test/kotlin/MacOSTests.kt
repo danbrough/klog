3ea557f818f6fc3b9fa71652eaf630417f54aeb2
Dan Brough <dan@danbrough.org>
2022-06-22 21:26:44 +1200

    update

--- src/macosX64Test/kotlin/MacOSTests.kt
+++ src/macosX64Test/kotlin/MacOSTests.kt
@@ -1,10 +1,11 @@
-import org.danbrough.klog.KMessageFormatter
+import org.danbrough.klog.KMessageFormatters
 import org.danbrough.klog.Level
+import org.danbrough.klog.colored
 import org.danbrough.klog.klog
 import kotlin.test.Test
 
 class MacOSTests {
-  private val log = klog(level = Level.TRACE, formatters = KMessageFormatters.verbose.colored)
+  private val log = klog(level = Level.TRACE, messageFormatter = KMessageFormatters.verbose.colored)
 
   @Test
   fun test1() {
