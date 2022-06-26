4c19fedf571762f988398132fde1217bf47a7cf7
Dan Brough <dan@danbrough.org>
2022-06-27 10:01:15 +1200

    update

--- src/macosX64Test/kotlin/MacOSTests.kt
+++ src/macosX64Test/kotlin/MacOSTests.kt
@@ -1,12 +1,26 @@
 import org.danbrough.klog.*
 import kotlin.test.Test
 
+
+
+
 class MacOSTests {
-  private val log = klog(level = Level.TRACE, messageFormatter = KMessageFormatters.verbose.colored)
 
   @Test
   fun test1() {
-    log.info("MacOS test working")
+    println("test1()")
+    println("klogname: ${this::class.klogName()}")
+    val log = klog(
+      level = Level.TRACE,
+      messageFormatter = KMessageFormatters.verbose.colored,
+      writer = KLogWriters.stdOut
+    )
+    println("LOG: $log")
 
+    log.info("an info message")
+
+    log.trace {
+      "A lazy trace message"
+    }
   }
 }
