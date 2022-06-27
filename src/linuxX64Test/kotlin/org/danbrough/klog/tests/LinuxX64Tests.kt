4c0193dfe6ac48d2c1dd82df1d2a03c27f125700
Dan Brough <dan@danbrough.org>
2022-06-27 12:22:00 +1200

    update

--- src/linuxX64Test/kotlin/org/danbrough/klog/tests/LinuxX64Tests.kt
+++ src/linuxX64Test/kotlin/org/danbrough/klog/tests/LinuxX64Tests.kt
@@ -5,18 +5,19 @@ import org.danbrough.klog.*
 import kotlin.test.Test
 
 class LinuxX64Tests {
+  private val log =
+    klog(
+      level = Level.TRACE,
+      KLogWriters.stdOut,
+      KMessageFormatters.verbose.colored
+    )
+
 
   @Test
   fun test1() {
-    println("test1()")
+    println("test1()!!")
+    println("regtistry: $kLogRegistry")
     println("klogname: ${this::class.klogName()}")
-    val log by lazy {
-      klog(
-        level = Level.TRACE,
-        KLogWriters.stdOut,
-        KMessageFormatters.verbose.colored
-      )
-    }
     println("LOG: $log")
 
     log.info("an info message")
--- src/posixMain/kotlin/org/danbrough/klog/posix.kt
+++ src/posixMain/kotlin/org/danbrough/klog/posix.kt
@@ -11,24 +11,9 @@ actual inline fun platformStatementContext(): StatementContext =
 
 actual fun getTimeMillis(): Long = org.danbrough.klog.posix.timeInMillisSinceEpoch().toLong()
 
-private val log = klog("org.danbrough.klog")
+private object NativeRegistry : DefaultLogRegistry()
+
+actual fun createKLogRegistry(): KLogRegistry = NativeRegistry
 
 
-//actual fun getTimeMillis(): Long = -1
-
-
-internal class NativeLogRegistry : DefaultLogRegistry() {
-}
-
-/*
-private
-var registry: DefaultLogRegistry? = null
-*/
-
-private val nativeLogRegistry by lazy {
-  println("Creating NativeLogRegistry")
-  NativeLogRegistry()
-}
-
-actual fun createKLogRegistry(): KLogRegistry = nativeLogRegistry
 actual fun KClass<*>.klogName(): String = qualifiedName!!.removeSuffix(".Companion")
