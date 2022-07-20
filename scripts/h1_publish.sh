9b6c78b11a11ec2e757569557301354fc1e4c116
Dan Brough <dan@danbrough.org>
2022-07-20 13:15:40 +1200

    update

--- scripts/h1_publish.sh
+++ scripts/h1_publish.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-cd $(dirname $0) && cd ..
+cd "$(dirname "$0")" && cd ..
 
 #BUILD_VERSION=$(./gradlew  -q buildVersion)
 #BUILD_VERSION_NAME=$(./gradlew  -q buildVersionName)
-VERSION_NAME=$(./gradlew -q buildVersionIncrement)
-echo publishing $VERSION_NAME
-gitsave
+
+function increment_version() {
+  VERSION_NAME=$(./gradlew -q buildVersionIncrement)
+  echo publishing "$VERSION_NAME"
+  gitsave
+}
 
 
 ssh -t mac "$( cat << CMD
 cd workspace/klog
 git pull
-./gradlew clean
+rm -rf build/m2
 ./gradlew  publishMacosArm64PublicationToM2Repository  publishMacosX64PublicationToM2Repository
 CMD
-)"
+)" || exit 1
+
+rm -rf build/m2
+./gradlew publishAllPublicationsToM2Repository || exit 1
+rsync -avHSx mac:~/workspace/klog/build/m2/ ./build/m2/ || exit 1
+rsync -avHSx ./build/m2/ h1:/srv/https/maven/
 
 #./gradlew publishAllPublicationsToM2Repository $@
 #rsync -avHSx ./build/m2/ h1:/srv/https/maven/
