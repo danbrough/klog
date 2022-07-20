eadacac84347418c29f8ec76e172b4af8caa50e3
Dan Brough <dan@danbrough.org>
2022-07-20 13:21:15 +1200

    update

--- scripts/h1_publish.sh
+++ scripts/h1_publish.sh
@@ -2,28 +2,28 @@
 
 cd "$(dirname "$0")" && cd ..
 
-#BUILD_VERSION=$(./gradlew  -q buildVersion)
-#BUILD_VERSION_NAME=$(./gradlew  -q buildVersionName)
-
 function increment_version() {
   VERSION_NAME=$(./gradlew -q buildVersionIncrement)
   echo publishing "$VERSION_NAME"
   gitsave
 }
 
+function publish_mac(){
+  git pull
+  rm -rf build/m2
+  ./gradlew  publishMacosArm64PublicationToM2Repository  publishMacosX64PublicationToM2Repository || exit 1
+  rsync -avHSx ./build/m2/ h1:/srv/https/maven/
+}
 
-ssh -t mac "$( cat << CMD
-cd workspace/klog
-git pull
-rm -rf build/m2
-./gradlew  publishMacosArm64PublicationToM2Repository  publishMacosX64PublicationToM2Repository
-CMD
-)" || exit 1
+function publish_linux() {
+  gitsave
+  rm -rf build/m2
+  ./gradlew publishAllPublicationsToM2Repository || exit 1
+  rsync -avHSx mac:~/workspace/klog/build/m2/ ./build/m2/ || exit 1
+}
 
-rm -rf build/m2
-./gradlew publishAllPublicationsToM2Repository || exit 1
-rsync -avHSx mac:~/workspace/klog/build/m2/ ./build/m2/ || exit 1
-rsync -avHSx ./build/m2/ h1:/srv/https/maven/
-
-#./gradlew publishAllPublicationsToM2Repository $@
-#rsync -avHSx ./build/m2/ h1:/srv/https/maven/
+if [ "$MACHTYPE" = "x86_64-apple-darwin21" ]; then
+  publish_mac
+else
+  publish_linux
+fi
