8f2b0af99a436ed9f2c4ba57689c63a890ded656
Dan Brough <dan@danbrough.org>
2022-07-24 17:27:22 +1200

    update

--- scripts/publishSnapshot.sh
+++ scripts/publishSnapshot.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+if is_mac; then
+  ./gradlew -PpublishDocs -PsignPublications publishMacTargetsToOSSRepository
+  exit 0
+fi
 
-cd `dirname $0` && cd ..
+cd $(dirname $0) && cd ..
 source scripts/common.sh
 
 #arrIN=(${IN//;/ })
-
 
 set_gradle_prop build.snapshot true
 
@@ -14,19 +17,16 @@ VERSION_NAME=$(./gradlew -q buildVersionNameNext)
 echo Creating release: $VERSION_NAME
 
 while true; do
-    read -p "Do you wish to proceed?: " yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
+  read -p "Do you wish to proceed?: " yn
+  case $yn in
+  [Yy]*) break ;;
+  [Nn]*) exit ;;
+  *) echo "Please answer yes or no." ;;
+  esac
 done
 
 git add .
 git commit -am "$VERSION_NAME"
 
-if is_mac; then
-  ./gradlew   -PpublishDocs -PsignPublications publishMacTargetsToOSSRepository
-else
-  ./gradlew publishAllPublicationsToOssRepository  -PpublishDocs -PsignPublications
-fi
+./gradlew publishAllPublicationsToOssRepository -PpublishDocs -PsignPublications
+
