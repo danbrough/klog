be2d6a899e788d902e5e575f245e8d861e037ed8
Dan Brough <dan@danbrough.org>
2023-08-31 16:21:32 +1200

    0.0.2-beta04

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,9 @@ pluginManagement {
 
 plugins {
   id("de.fayard.refreshVersions") version "0.51.0"
+////                          # available:"0.60.0"
+////                          # available:"0.60.1"
+////                          # available:"0.60.2"
 }
 
 
