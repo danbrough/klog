b1561f806d6ca10d5cde71fcb622cb358f84d19a
Dan Brough <dan@danbrough.org>
2024-05-13 18:28:55 +1200

    update

--- README.md
+++ README.md
@@ -9,13 +9,22 @@ the [Android Log](https://developer.android.com/reference/android/util/Log) on a
 Alternatively [kotlin-logging](https://github.com/oshai/kotlin-logging) is available as well
 or you can trivially wire it up to anything with a similar api.
 
-```kotlin 
+```kotlin
+
+/**
+ *Initial demo builds are available via maven at:
+ * repositories {
+ *    maven("https://maven.danbrough.org")
+ * }
+ * implementation("org.danbrough.klog:klog:0.0.3-beta02")
+ */
+
 
 import klog.logging
 
 class Foo {
 	private val log by logging()
-  
+
 	fun test() {
 		log.trace { "A trace call" } //or debug,info,warn,error ..
 		log.error(Exception("An exception")) { "An error occurred" }
--- build.gradle.kts
+++ build.gradle.kts
@@ -9,167 +9,165 @@ import org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTarget
 import org.jetbrains.kotlin.konan.target.HostManager
 
 plugins {
-	alias(libs.plugins.kotlin.multiplatform)
-	alias(libs.plugins.android.library)
-	id("org.danbrough.klog.support")
-	`maven-publish`
-	signing
-
+  alias(libs.plugins.kotlin.multiplatform)
+  alias(libs.plugins.android.library)
+  id("org.danbrough.klog.support")
+  `maven-publish`
+  signing
 }
 
 group = Constants.KLOG_PACKAGE
 version = "0.0.3-beta01"
 
 repositories {
-	mavenCentral()
-	google()
+  mavenCentral()
+  google()
 }
 
 kotlin {
-	applyDefaultHierarchyTemplate()
+  applyDefaultHierarchyTemplate()
 
-//	if (HostManager.hostIsLinux) {
-		mingwX64()
-		linuxX64()
-		linuxArm64()
-		androidNativeX64()
-		androidNativeArm64()
-//	}
-
-	if (HostManager.hostIsMac) {
-		macosArm64()
-		macosX64()
-		iosArm64()
-		iosX64()
-	}
-
-	jvm {
-		compilerOptions {
-			jvmTarget = Constants.JVM_TARGET
-		}
-	}
-
-	androidTarget {
-		publishLibraryVariants("release")
-
-		@OptIn(ExperimentalKotlinGradlePluginApi::class)
-		compilerOptions {
-			jvmTarget = Constants.JVM_TARGET
-		}
-	}
-
-	compilerOptions {
-		languageVersion = KotlinVersion.KOTLIN_1_9
-		apiVersion = KotlinVersion.KOTLIN_1_9
-		freeCompilerArgs = listOf("-Xexpect-actual-classes")
-	}
-
-	sourceSets {
-
-		val commonMain by getting {
-			dependencies {
-				implementation(kotlin("reflect"))
-				implementation(libs.oshai.logging)
-			}
-		}
-
-		commonTest {
-			dependencies {
-				implementation(kotlin("test"))
-			}
-		}
-
-		val jvmAndroidMain by creating {
-			dependsOn(commonMain)
-			dependencies {
-				implementation(libs.slf4j.api)
-			}
-		}
-
-		androidMain {
-			dependsOn(jvmAndroidMain)
-		}
-
-		val androidInstrumentedTest by getting {
-			dependencies {
-				implementation(kotlin("test-junit"))
-				implementation(libs.androidx.test.runner)
-			}
-		}
-
-		jvmMain {
-			dependsOn(jvmAndroidMain)
-		}
-
-		jvmTest {
-			dependencies {
-				implementation(libs.logback.classic)
-			}
-		}
-
-		nativeMain {
-			dependencies {
-			}
-		}
-	}
+  mingwX64()
+  linuxX64()
+  linuxArm64()
+  androidNativeX64()
+  androidNativeArm64()
 
 
-	targets.withType<KotlinJvmTarget> {
-		mainRun {
-			mainClass = "klog.TestApp"
-			classpath(compilations["test"])
-		}
-	}
+  if (HostManager.hostIsMac) {
+    macosArm64()
+    macosX64()
+    iosArm64()
+    iosX64()
+  }
+
+  jvm {
+    compilerOptions {
+      jvmTarget = Constants.JVM_TARGET
+    }
+  }
+
+  androidTarget {
+    publishLibraryVariants("release")
+
+    @OptIn(ExperimentalKotlinGradlePluginApi::class)
+    compilerOptions {
+      jvmTarget = Constants.JVM_TARGET
+    }
+  }
+
+  compilerOptions {
+    languageVersion = KotlinVersion.KOTLIN_1_9
+    apiVersion = KotlinVersion.KOTLIN_1_9
+    freeCompilerArgs = listOf("-Xexpect-actual-classes")
+  }
+
+  sourceSets {
+
+    val commonMain by getting {
+      dependencies {
+        implementation(kotlin("reflect"))
+        implementation(libs.oshai.logging)
+      }
+    }
+
+    commonTest {
+      dependencies {
+        implementation(kotlin("test"))
+      }
+    }
+
+    val jvmAndroidMain by creating {
+      dependsOn(commonMain)
+      dependencies {
+        implementation(libs.slf4j.api)
+      }
+    }
+
+    androidMain {
+      dependsOn(jvmAndroidMain)
+    }
+
+    val androidInstrumentedTest by getting {
+      dependencies {
+        implementation(kotlin("test-junit"))
+        implementation(libs.androidx.test.runner)
+      }
+    }
+
+    jvmMain {
+      dependsOn(jvmAndroidMain)
+    }
+
+    jvmTest {
+      dependencies {
+        implementation(libs.logback.classic)
+      }
+    }
+
+    nativeMain {
+      dependencies {
+      }
+    }
+  }
+
+
+  targets.withType<KotlinJvmTarget> {
+    mainRun {
+      mainClass = "klog.TestApp"
+      classpath(compilations["test"])
+    }
+  }
 }
 
 
 
 android {
-	compileSdk = Constants.Android.COMPILE_SDK
-	namespace = project.group.toString()
+  compileSdk = Constants.Android.COMPILE_SDK
+  namespace = project.group.toString()
 
-	defaultConfig {
-		minSdk = Constants.Android.MIN_SDK
-		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-	}
+  defaultConfig {
+    minSdk = Constants.Android.MIN_SDK
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
 
-	compileOptions {
-		sourceCompatibility = Constants.JAVA_VERSION
-		targetCompatibility = Constants.JAVA_VERSION
-	}
+  compileOptions {
+    sourceCompatibility = Constants.JAVA_VERSION
+    targetCompatibility = Constants.JAVA_VERSION
+  }
 }
 
 publishing {
-	repositories {
-		maven(rootProject.layout.buildDirectory.asFile.get().resolve("m2")) {
-			name = "Local"
-		}
-	}
+  repositories {
+    maven(rootProject.layout.buildDirectory.asFile.get().resolve("m2")) {
+      name = "Local"
+    }
+  }
 
-	signing {
-		publications.all {
-			sign(this)
-		}
-	}
+  signing {
+    publications.all {
+      sign(this)
+    }
+  }
 }
 
 
 tasks.withType<AbstractTestTask> {
-	if (this is Test) {
-		useJUnitPlatform()
-	}
+  if (this is Test) {
+    useJUnitPlatform()
+  }
 
-	testLogging {
-		events = setOf(
-			TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED
-		)
-		showStandardStreams = true
-		showStackTraces = true
-		exceptionFormat = TestExceptionFormat.FULL
-	}
+  testLogging {
+    events = setOf(
+      TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED
+    )
+    showStandardStreams = true
+    showStackTraces = true
+    exceptionFormat = TestExceptionFormat.FULL
+  }
 
-	outputs.upToDateWhen {
-		false
-	}
+  outputs.upToDateWhen {
+    false
+  }
 }
 
