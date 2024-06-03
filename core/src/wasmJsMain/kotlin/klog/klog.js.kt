8c50b4c00f4ff7efe517823ccb80c940b7d994ca
Dan Brough <dan@danbrough.org>
2024-06-04 08:30:49 +1200

    update

--- core/src/wasmJsMain/kotlin/klog/klog.js.kt
+++ core/src/wasmJsMain/kotlin/klog/klog.js.kt
@@ -1,23 +1,11 @@
 package klog
 
 import klog.stdout.StdoutLogging
-import klog.stdout.colorString
 import kotlin.reflect.KClass
 
-object JsLogFactory : KLogFactory() {
 
-	init {
-		StdoutLogging.coloredOutput = false
-	}
+var loggerName = "klog"
 
-	private var log: LoggerMethod = { level, name, message, t ->
-		console.log(StdoutLogging.formatter.invoke(level, name, message))
-		if (t != null) console.log(colorString(Logger.Level.ERROR, t.stackTraceToString()))
-	}
+actual fun kloggingDefault(): KLogFactory = StdoutLogging
 
-	override fun logger(logName: String) = LoggerImpl(logName, log)
-}
-
-internal actual fun kloggingDefault(): KLogFactory = JsLogFactory
-
-actual fun <T : Any> loggerName(clazz: KClass<T>): String = "KOTLIN"
+actual fun <T : Any> loggerName(clazz: KClass<T>): String = loggerName
