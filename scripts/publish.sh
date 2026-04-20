995c947e67325b36b1668a2f27b8e02ea5ace8c7
Dan Brough <dan@danbrough.org>
2026-04-20 11:26:51 -0600

    update

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,9 +6,9 @@ cd "$(dirname "$0")" && cd ..
 
 #./gradlew publishAllPublicationsToLocal || exit 1
 
-
+rm -rf /Users/dan/workspace/xtras/xtras/maven/org/danbrough/klog/
 #rsync -avHSx ./build/m2/ maven@h1:~/m2/
 ./gradlew publishAllPublicationsToXtras || exit 1
 
-rm -rf /Users/dan/workspace/xtras/xtras/maven/org/danbrough/klog/
+
 rsync -avHSx /Users/dan/workspace/xtras/xtras/maven/org/danbrough/klog/ maven:~/m2/org/danbrough/klog/
