5feca045ba7733fddab71278cd848280de05383d
Dan Brough <dan@danbrough.org>
2026-08-21 10:34:40 -0600

    0.1.4

--- core/src/androidNativeMain/kotlin/org/danbrough/klog/Utils.androidNative.kt
+++ core/src/androidNativeMain/kotlin/org/danbrough/klog/Utils.androidNative.kt
@@ -1,9 +1,25 @@
 package org.danbrough.klog
 
+import platform.android.__android_log_print
+
+
+val Level.androidLogLevel: Int
+  get() = when (this) {
+    Level.TRACE -> 2
+    Level.DEBUG -> 3
+    Level.INFO -> 4
+    Level.WARN -> 5
+    Level.ERROR -> 6
+    else -> -1
+  }
 
 val AndroidLogWriter: KLogWriter =
   { conf: KLogConfiguration, level: Level, name: String, message: String, t: Throwable? ->
-    //level.androidLogMethod(name, message, t)
+    __android_log_print(
+      level.androidLogLevel,
+      name,
+      if (t != null) "$message: ${t.stackTraceToString()}" else message
+    )
   }
 
 object AndroidNativeLogFactory : KLogFactory(KLogConfiguration(AndroidLogWriter)) {
